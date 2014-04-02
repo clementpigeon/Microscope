bc5fc7122974470cd630b69e298bbcef03c218e2
Clement Pigeon <clementpigeon@gmail.com>
2014-04-02 22:12:00 +0200

    add router, layout

--- client/main.js
+++ client/main.js
@@ -1,1 +1,10 @@
 Meteor.subscribe('posts', "me");
+
+Router.configure({
+	layoutTemplate: 'layout'
+})
+
+Router.map(function(){
+	this.route('postsList');
+	// this.route('postsList', {path: "/"});
+})
