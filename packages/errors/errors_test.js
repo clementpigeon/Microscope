78b1de439f8865e2607d6027fbf920e4b9fdfdda
Clement Pigeon <clementpigeon@gmail.com>
2014-05-06 23:05:03 +0200

    fix tests

--- packages/errors/errors_test.js
+++ packages/errors/errors_test.js
@@ -10,9 +10,8 @@ Tinytest.add('Errors collection works', function(test){
 
 Tinytest.addAsync('Errors template works', function(test, done){
 
-  Errors.throw('an error');
+  Errors.throw('a new error');
   test.equal(Errors.collection.find({seen: false}).count(), 1);
-
 
   //renders the template
   UI.insert(UI.render(Template.meteorErrors), document.body);
@@ -24,11 +23,8 @@ Tinytest.addAsync('Errors template works', function(test, done){
 
     Errors.clearSeen();
 
-    test.equal(Errors.collection.find({}).count(), 0);
+    test.equal(Errors.collection.find({seen: true}).count(), 0);
     done();
   }, 500)
 
-
-
-  Errors.collection.remove({});
 });
